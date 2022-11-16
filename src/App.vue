d51137c1f3d3555fa7b2dd31b9c84e097e17428f
Matteogatto94 <cosenza94@live.it>
2022-11-16 14:20:17 +0100

    add correction milestone 0

--- src/App.vue
+++ src/App.vue
@@ -8,21 +8,23 @@ export default {
       store
     }
   },
-  methods: {
-    callApi(url) {
-      axios.get(url)
-        .then(response => {
-          console.log(response);
-          this.store.movies = response.data.results
-        })
-        .catch(err => {
-          console.error(err.message);
-          this.store.error = err.message
-        })
-    }
-  },
   mounted() {
-    this.callApi(this.store.API_URL)
+    const config = {
+      method: 'get',
+      url: 'https://api.themoviedb.org/3/search/movie',
+      params: {
+        api_key: 'ef7e66d24e6adde17d381d55ad3ecd29',
+        query: 'Matrix'
+      }
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      })
   }
 }
 </script>
--- src/store.js
+++ src/store.js
@@ -3,7 +3,6 @@ import {
 } from 'vue'
 
 export const store = reactive({
-    API_URL: 'https://api.themoviedb.org/3/search/movie?api_key=ef7e66d24e6adde17d381d55ad3ecd29&language=en-US&query=Matrix&page=1&include_adult=false',
     movies: null,
-    error: null,
+    error: null
 })
