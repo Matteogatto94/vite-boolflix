d252534a0f439d708b737b49e84f050d8f8e363f
Matteogatto94 <cosenza94@live.it>
2022-11-17 22:42:31 +0100

    add header logo and bg

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,16 +11,29 @@ export default {
 </script>
 
 <template>
-    <div class="container justify-content-center">
-        <div class="search d-flex gap-2 my-5">
-            <input type="search" class="form-control w-50" v-model="store.searchMovies"
-                @keyup.enter="store.callApi(store.searchMovies)" placeholder="Search a Movie...">
-            <button type="submit" class="btn btn-dark" @click="store.callApi(store.searchMovies)">Search</button>
-        </div>
-    </div>
+    <header>
+        <section class="bg-dark mb-5">
+            <div class="container d-flex justify-content-between">
+                <div class="logo">
+                    <h1>BOOLFIX</h1>
+                </div>
+                <div class="search d-flex gap-2 my-5">
+                    <input type="search" class="form-control" v-model="store.searchMovies"
+                        @keyup.enter="store.callApi(store.searchMovies)" placeholder="Search a Movie...">
+                    <button type="submit" class="btn btn-danger"
+                        @click="store.callApi(store.searchMovies)">Search</button>
+                </div>
+            </div>
+        </section>
+
+    </header>
+
 
 </template>
 
 <style lang="scss">
-
+.logo {
+    align-self: center;
+    color: red;
+}
 </style>
