7da4e5087457f76b8735529f569fc741d1940b0b
Matteogatto94 <cosenza94@live.it>
2022-11-17 14:37:00 +0100

    init milestone 3 add image and import fontawesome

--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -12,20 +12,40 @@ export default {
 
 <template>
     <div class="col" v-for="movie in store.movies">
-        <p>{{ movie.title }}</p>
-        <p v-if="movie.title != movie.original_title">{{ movie.original_title }}</p>
-        <img :src="store.flagsCountry(movie.original_language)">
-        <p>{{ movie.vote_average }}</p>
+        <div class="card">
+            <div class="card-img text-center">
+                <img :src="'https://image.tmdb.org/t/p/w185/' + movie.poster_path">
+            </div>
+            <div class="card-body">
+                <p>Titolo: {{ movie.title }}</p>
+                <p v-if="movie.title != movie.original_title">Titolo Originale: {{ movie.original_title }}</p>
+                <img :src="store.flagsCountry(movie.original_language)">
+                <p>Voto: {{ movie.vote_average }}</p>
+            </div>
+        </div>
     </div>
 
     <div class="col" v-for="movie in store.tvSeries">
-        <p>{{ movie.name }}</p>
-        <p v-if="movie.name != movie.original_name">{{ movie.original_name }}</p>
-        <img :src="store.flagsCountry(movie.original_language)">
-        <p>{{ movie.vote_average }}</p>
+        <div class="card">
+            <div class="card-img">
+                <img :src="'https://image.tmdb.org/t/p/w185/' + movie.poster_path">
+            </div>
+            <div class="card-body">
+                <p>Titolo: {{ movie.name }}</p>
+                <p v-if="movie.name != movie.original_name">Titolo Originale: {{ movie.original_name }}</p>
+                <img :src="store.flagsCountry(movie.original_language)">
+                <p>Voto: {{ movie.vote_average }}</p>
+            </div>
+        </div>
     </div>
+
+
+
+
 </template>
 
 <style lang="scss">
-
+.card {
+    padding: 2rem;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import './assets/scss/style.scss'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+library.add(faUserSecret)
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
