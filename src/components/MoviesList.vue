5b02e99f94dc9833cb5cdcc4b2dccc844c13b46f
Matteogatto94 <cosenza94@live.it>
2022-11-17 22:30:36 +0100

    finish milestone 3

--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -12,29 +12,39 @@ export default {
 
 <template>
     <div class="col" v-for="movie in store.movies">
-        <div class="card">
-            <div class="card-img text-center">
+        <div class="movie_card text-center">
+            <div class="card-img">
                 <img :src="'https://image.tmdb.org/t/p/w185/' + movie.poster_path">
             </div>
             <div class="card-body">
                 <p>Titolo: {{ movie.title }}</p>
                 <p v-if="movie.title != movie.original_title">Titolo Originale: {{ movie.original_title }}</p>
+                <div class="rating">
+                    <span>Voto:</span>
+                    <font-awesome-icon icon="fa-solid fa-star" v-for="(i) in 5"
+                        :class="Math.ceil(movie.vote_average / 2) > i ? 'yellow' : 'black'" />
+                </div>
                 <img :src="store.flagsCountry(movie.original_language)">
-                <p>Voto: {{ movie.vote_average }}</p>
+                <p>{{ movie.overview }}</p>
             </div>
         </div>
     </div>
 
     <div class="col" v-for="movie in store.tvSeries">
-        <div class="card">
+        <div class="movie_card  text-center">
             <div class="card-img">
                 <img :src="'https://image.tmdb.org/t/p/w185/' + movie.poster_path">
             </div>
             <div class="card-body">
                 <p>Titolo: {{ movie.name }}</p>
                 <p v-if="movie.name != movie.original_name">Titolo Originale: {{ movie.original_name }}</p>
+                <div class="rating">
+                    <span>Voto:</span>
+                    <font-awesome-icon icon="fa-solid fa-star" v-for="(i) in 5"
+                        :class="Math.ceil(movie.vote_average / 2) > i ? 'yellow' : 'black'" />
+                </div>
                 <img :src="store.flagsCountry(movie.original_language)">
-                <p>Voto: {{ movie.vote_average }}</p>
+                <p></p>
             </div>
         </div>
     </div>
@@ -45,7 +55,14 @@ export default {
 </template>
 
 <style lang="scss">
-.card {
-    padding: 2rem;
+.rating {
+    .yellow {
+        color: yellow;
+    }
+
+    .black {
+        color: black;
+    }
+
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import './assets/scss/style.scss'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 library.add(faUserSecret)
+library.add(faStar)
 import App from './App.vue'
 
 createApp(App)
